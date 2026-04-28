bb0c0e95522822d8a3816dea79a628cd87427225
sravya-113 <sravyavaranasi2005@gmail.com>
2026-04-29 02:54:23 +0530

    fix: completely remove manualChunks to fix build

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor';
-            }
-            if (id.includes('framer-motion')) {
-              return 'framer';
-            }
-          }
-        }
-      }
-    }
+    // relying on Vite's default chunking strategy
   },
   server: {
     port: 3000,
